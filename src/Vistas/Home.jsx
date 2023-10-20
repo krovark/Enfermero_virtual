2f60f606142f476c47834034f3c9cb99c28874d4
krovark <santglez51@gmail.com>
2023-10-20 02:04:24 -0300

    Borré lo de home porque era ReactJS, no era Native

--- src/Vistas/Home.jsx
+++ src/Vistas/Home.jsx
@@ -1,33 +1,21 @@
 import React from 'react';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
 import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  const [Fecha, setFecha] = useState(new Date());
-
-  const [minDate, setMinDate] = useState (new Date());
-
-    return (
-      <>
-        <DatePicker 
-        selected={Fecha} 
-        showPreviousMonths
-        monthsShown={2}
-        minDate={minDate}
-        withPortal
-
-        />
-      </>
-    );
-  };
   
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
+  return (
+    <View style={styles.container}>
+      <Text>Hola Hola hola hola mundo</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
   
   export default HomeScreen;
