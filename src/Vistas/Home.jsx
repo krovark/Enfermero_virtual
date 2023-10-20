0255b04fbddaf023d36c97bee89a17d06b710fe3
fedemartin25 <107190447+fedemartin25@users.noreply.github.com>
2023-10-19 21:51:36 -0300

    cambio

--- src/Vistas/Home.jsx
+++ src/Vistas/Home.jsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+
 import { View, Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  const [Fecha, setFecha] = useState(new Date());
+  
 
-  const [minDate, setMinDate] = useState (new Date());
-
-    return (
-      <>
-        <DatePicker 
-        selected={Fecha} 
-        showPreviousMonths
-        monthsShown={2}
-        minDate={minDate}
-        withPortal
-
-        />
-      </>
-    );
+    
   };
   
   const styles = StyleSheet.create({
