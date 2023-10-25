e2bf532c8e1eff1c9bd2593a2053c8154e2deee8
Luis <laclvl99@gmail.com>
2023-10-25 14:44:06 -0300

    Vista Andres

--- src/Vistas/RTratamiento.jsx
+++ src/Vistas/RTratamiento.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button  } from 'react-native';
 
 const Tratamientos = () => {
+  const [text, setText] = useState(''); // State to hold the input text
+
+  const handleSubmit = () => {
+    // Do something with the user's input (e.g., save it to a variable or send it to a server)
+    console.log('User input:', text);
+    };
     return (
-      <View style={styles.container}>
-        <Text>Hola Hola hola hola mundo</Text>
-      </View>
+      <View>
+      <Text>Recordatorio de tratamiento</Text>
+      <TextInput
+        label="Tratamiento"
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(input) => setText(input)}
+        value={text}
+      />
+      <Text>Horario</Text>
+      <TextInput
+        label="Horario"
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(input) => setText(input)}
+        value={text}
+      />
+      <Text>Notas</Text>
+      <TextInput
+        label="Notas"
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(input) => setText(input)}
+        value={text}
+      />
+      <Text>Alarma</Text>
+      <Button
+        title="Submit"
+        onPress={handleSubmit}
+      />
+    </View>
     );
   };
   
