858fb8e4743dc41358a77889dc17d0dac31dffa3
JoaquinHaedo <66391530+JoaquinHaedo@users.noreply.github.com>
2023-10-28 12:43:01 -0300

    login y register

--- src/Vistas/LoginRegister/Login.jsx
+++ src/Vistas/LoginRegister/Login.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, ImageBackground } from 'react-native';
 import { Button, TextInput as TextInputPaper } from 'react-native-paper';
+import { IconButton } from 'react-native-paper';
 
-const Login = () => {
+const Login = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -14,6 +15,13 @@ const Login = () => {
   return (
     <ImageBackground source={{ uri: "https://img.freepik.com/foto-gratis/hermosa-joven-doctora-mirando-camara-oficina_1301-7807.jpg" }} style={{ flex: 1, width: '100%', height: '100%' }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.4)' }}>
+        <IconButton
+        icon="arrow-left"
+        size={30}
+        style={styles.iconButton}
+        iconColor='black'
+        onPress={() => navigation.navigate('Inicio')}
+        />
         <Text style={{ fontSize: 24, color: 'white' }}>Iniciar Sesión</Text>
         <View style={{ width: '80%' }}>
           <TextInputPaper
@@ -46,6 +54,11 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+  },
+  iconButton: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
   },
 });
 
--- src/Vistas/LoginRegister/Register.jsx
+++ src/Vistas/LoginRegister/Register.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ImageBackground, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground, Alert } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+import { IconButton } from 'react-native-paper';
+
+
 
 const Register = ({ navigation }) => {
+  const [step, setStep] = useState(1);
   const [nombre, setNombre] = useState('');
   const [apellido, setApellido] = useState('');
   const [genero, setGenero] = useState('');
@@ -13,7 +18,7 @@ const Register = ({ navigation }) => {
 
   const handleSubmit = () => {
     if (password !== confirmPassword) {
-      alert('Las contraseñas no coinciden');
+      Alert.alert('Las contraseñas no coinciden');
       return;
     }
 
@@ -33,16 +38,100 @@ const Register = ({ navigation }) => {
   return (
     <ImageBackground source={{ uri: "https://img.freepik.com/foto-gratis/doctor-trabajo_144627-40457.jpg" }} style={{ flex: 1, width: '100%', height: '100%' }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0,0,0,0.4)' }}>
+        <IconButton
+        icon="arrow-left"
+        size={30}
+        style={styles.iconButton}
+        iconColor='black'
+        onPress={() => navigation.navigate('Inicio')}
+        />
         <Text style={{ fontSize: 24, color: 'white' }}>Registrarse</Text>
         <View style={{ width: '80%' }}>
-          <TextInput
-            placeholder="Nombre"
-            value={nombre}
-            onChangeText={setNombre}
-            style={styles.input}
-          />
-          {/* Repite este patrón para los demás campos */}
-          <Button title="Registrarse" onPress={handleSubmit} />
+          {step === 1 && (
+            <>
+              <TextInput
+                label="Nombre"
+                value={nombre}
+                onChangeText={setNombre}
+                style={styles.input}
+                mode="outlined"
+              />
+              <TextInput
+                label="Apellido"
+                value={apellido}
+                onChangeText={setApellido}
+                style={styles.input}
+                mode="outlined"
+              />
+              <TextInput
+                label="Género"
+                value={genero}
+                onChangeText={setGenero}
+                style={styles.input}
+                mode="outlined"
+              />
+              <Button mode="contained" onPress={() => setStep(2)} style={styles.button}>
+                Siguiente
+              </Button>
+            </>
+          )}
+          {step === 2 && (
+            <>
+              <TextInput
+                label="DNI"
+                value={dni}
+                onChangeText={setDni}
+                style={styles.input}
+                mode="outlined"
+              />
+              <TextInput
+                label="Fecha de nacimiento"
+                value={fechaNacimiento}
+                onChangeText={setFechaNacimiento}
+                style={styles.input}
+                mode="outlined"
+              />
+              <TextInput
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                style={styles.input}
+                mode="outlined"
+              />
+              <Button mode="contained" onPress={() => setStep(3)} style={styles.button}>
+                Siguiente
+              </Button>
+              <Button mode="contained" onPress={() => setStep(1)} style={styles.button} buttonColor="#D3D3D3">
+                Atrás
+              </Button>
+            </>
+          )}
+          {step === 3 && (
+            <>
+              <TextInput
+                label="Contraseña"
+                value={password}
+                onChangeText={setPassword}
+                style={styles.input}
+                mode="outlined"
+                secureTextEntry
+              />
+              <TextInput
+                label="Confirmar contraseña"
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                style={styles.input}
+                mode="outlined"
+                secureTextEntry
+              />
+              <Button mode="contained" onPress={handleSubmit} style={styles.button}>
+                Registrarse
+              </Button>
+              <Button mode="contained" onPress={() => setStep(2)} style={styles.button} buttonColor="#D3D3D3">
+                Atrás
+              </Button>
+            </>
+          )}
         </View>
       </View>
     </ImageBackground>
@@ -52,6 +141,14 @@ const Register = ({ navigation }) => {
 const styles = StyleSheet.create({
   input: {
     marginBottom: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  iconButton: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
   },
 });
 
