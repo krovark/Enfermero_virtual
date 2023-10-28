ff85b8392883707bff85a8b771ba9fdc9adb9fa5
fedemartin25 <107190447+fedemartin25@users.noreply.github.com>
2023-10-28 19:50:28 -0300

    cambios home

--- src/Vistas/Home.jsx
+++ src/Vistas/Home.jsx
@@ -1,19 +1,55 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import {Calendar, LocaleConfig} from 'react-native-calendars';
+import {Button} from 'react-native-paper';
 
 const HomeScreen = () => {
   const [selected, setSelected] = useState('');
 
+  const buttonContainerStyle = {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  }; 
+
+  const buttonStyle = {
+    margin: 15,
+    padding: 15,
+    fontSize: 18,
+  };
+
   return (
-    <Calendar
-      onDayPress={day => {
-        setSelected(day.dateString);
-      }}
-      markedDates={{
-        [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'violet'}
-      }}
-    />
+    <view>
+      <Calendar
+
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          height: 350
+        }}
+
+        theme={{
+          backgroundColor: '#ffffff',
+          calendarBackground: '#ffffff',
+          textSectionTitleColor: '#b6c1cd',
+          selectedDayBackgroundColor: '#00adf5',
+          selectedDayTextColor: '#ffffff',
+          todayTextColor: '#00adf5',
+          dayTextColor: '#2d4150',
+          textDisabledColor: '#d9e000'
+        }}
+
+        onDayPress={day => {
+          setSelected(day.dateString);
+        }}
+        markedDates={{
+          [selected]: {selected: true, disableTouchEvent: true, selectedDotColor: 'violet'}
+        }}
+      />
+      <view style={buttonContainerStyle}>
+        <button mode="contained" style={buttonStyle} buttonColor='red'>SOS</button>
+
+      </view>
+    </view>
   );
   };
   
