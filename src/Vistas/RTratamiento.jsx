53f7dc6245961d60a99d31f06d539fd96512fcc9
Luis <laclvl99@gmail.com>
2023-11-01 13:37:03 -0300

    Tratamineto_Andres_2

--- src/Vistas/RTratamiento.jsx
+++ src/Vistas/RTratamiento.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Button  } from 'react-native';
 
 const Tratamientos = () => {
-  const [text, setText] = useState(''); // State to hold the input text
-
+  const [tratamineto, setTratamiento] = useState(''); // State to hold the input text
+  const [horario, setHorario] = useState('');
+  const [notas, setNotas] = useState('');
   const handleSubmit = () => {
     // Do something with the user's input (e.g., save it to a variable or send it to a server)
-    console.log('User input:', text);
+    console.log('User input:', tratamineto);
     };
     return (
       <View>
@@ -14,22 +15,22 @@ const Tratamientos = () => {
       <TextInput
         label="Tratamiento"
         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(input) => setText(input)}
-        value={text}
+        onChangeText={(input) => setTratamiento(input)}
+        value={tratamineto}
       />
       <Text>Horario</Text>
       <TextInput
         label="Horario"
         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(input) => setText(input)}
-        value={text}
+        onChangeText={(input) => setHorario(input)}
+        value={horario}
       />
       <Text>Notas</Text>
       <TextInput
         label="Notas"
         style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(input) => setText(input)}
-        value={text}
+        onChangeText={(input) => setNotas(input)}
+        value={notas}
       />
       <Text>Alarma</Text>
       <Button
