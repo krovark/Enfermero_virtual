31531343dfa4dc20b5b5332602756d72010ce79e
fedemartin25 <107190447+fedemartin25@users.noreply.github.com>
2023-11-13 16:46:39 -0300

    controller, route y api de visitas medicas

--- Servidor/routes/api.js
+++ Servidor/routes/api.js
@@ -3,7 +3,9 @@ var express = require('express')
 
 var router = express.Router()
 var users = require('./api/user.route')
+var visitasmed = require('./api/visitasmed.route')
 
 router.use('/users', users);
+router.use('/visitasmed', visitasmed);
 
 module.exports = router;
